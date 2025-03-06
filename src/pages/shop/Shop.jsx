9efc159bc1a78700a56615276135e44f9198bd45
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-03-06 18:09:52 +0100

    Add all products component

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,23 +1,14 @@
 import { Link } from "react-router-dom";
+import AllProducts from "../../components/product/useProduct";
 import "./Shop.css";
 
 const Shop = () => {
-    return (
-        <div>
-        <h1>Shop</h1>
-        <ul>
-            <li>
-            <Link to="/product/1">Product 1</Link>
-            </li>
-            <li>
-            <Link to="/product/2">Product 2</Link>
-            </li>
-            <li>
-            <Link to="/product/3">Product 3</Link>
-            </li>
-        </ul>
-        </div>
-    );
-    }
+    return(
+    <div className="shop-display">
+        <AllProducts />
+    </div>
+    )
+};
+
 
 export default Shop;
