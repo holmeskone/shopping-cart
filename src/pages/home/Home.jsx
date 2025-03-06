6a2ff3234242433f8d062084e0d28bb8cbd77a36
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-03-06 18:09:15 +0100

    Remove emoji idea and convert into hero section with a cta and link to shop section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,19 @@
+import { Link } from "react-router-dom";
 import "./Home.css";
+import Product from "../../components/product/useProduct";
 const Home = () => {
   return (
     <div>
       <div className="hero">
       <h1>Welcome to mojis'</h1>
-      <h2 className="emoji">&#128507;</h2>
+      <h2>Discover our selection of styles.</h2>
+      <div className="cta">
+        <Link to="/shop">
+        <button type="button">Shop</button>
+        </Link>
+      </div>
+      <div>
+      </div>
       </div>
     </div>
   );
