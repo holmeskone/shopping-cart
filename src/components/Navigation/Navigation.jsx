25d1aac4dd65b31253691be7057fb59c4185aff4
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-03-07 11:18:59 +0100

    Add a conditional rendering that displays a full or empty cart depending on if the user added something to cart or not, I'm using states from App.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,21 +1,35 @@
 import { Link } from "react-router-dom";
 import "./Navigation.css";
-const Navigation = () => {
-    return (
-        <div className="header">
-            <nav>
-                <div>
-                    <Link to="/">mojis'</Link>
-                </div>
-                <div>
-                    <Link to="/shop">shop</Link>
-                </div>
-                <div>
-                    <Link to="/cart">cart'</Link>
-                </div>
-            </nav>
+
+const Navigation = ({cart, setCart}) => {
+  // Change: Remove the useEffect that was overriding the cart state
+  // The cart state should already be managed by App.jsx
+
+  function EmptyCart() {
+    return <i className="fa-solid fa-cart-shopping"></i>;
+  }
+
+  function FullCart() {
+    return <i className="fa-solid fa-cart-plus"></i>;
+  }
+
+  return (
+    <div className="header">
+      <nav>
+        <div>
+          <Link to="/">mojis'</Link>
         </div>
-    );
-    }
+        <div>
+          <Link to="/shop">shop</Link>
+        </div>
+        <div>
+          <Link to="/cart">
+            {cart.length > 0 ? <FullCart /> : <EmptyCart />} {/* Change: Check array length instead of boolean */}
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default Navigation;
